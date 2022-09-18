782e6a7d92134ef08bf471177987c6fc1a1a3efc
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-09-18 14:24:33 +1200

    Work pls image, i remane :pleading_face:

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-let port = 4000 || process.env.PORT
+let port = 3000 || process.env.PORT
 
 // route vars
 const contact = require("./src/routes/contact.js")
@@ -10,10 +10,6 @@ const impact = require("./src/routes/imapct.js")
 const newsandnotices = require("./src/routes/newsandnotices.js")
 
 app.use(express.static(path.join(__dirname+"/src/public")))
-app.use(express.static(path.join(__dirname+"/src/public/images")))
-app.use(express.static(path.join(__dirname+"/src/public/stylesheets")))
-app.use(express.static(path.join(__dirname+"/src/public/templates")))
-
 
 // serving the routes
 app.use("/contact", contact)
